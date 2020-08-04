b0b1a48b8954680df435d511d5ef533418e642c2
marcio944 <marcinho944@hotmail.com>
2020-08-04 10:50:54 -0300

    Projeto criado

---
+++ src/test/java/com/marcio/cursoJava/CursoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcio.cursoJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
